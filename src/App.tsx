a2a40aff3fa42209abbbd754a41ec913d83c8c24
Jaron Warren <jarwarren@gmail.com>
2023-06-29 21:32:55 -0600

    flex grid setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,28 @@ import "./App.css";
 function App() {
   return (
     <>
-      <header>
-        <div className="grid-cols-1">
-          <h1>H2O Time</h1>
-        </div>
-      </header>
-      <div id="schedule" className="">
-        <h2>Schedule:</h2>
-        <div>
+      <div className="flex min-h-screen flex-col gap-2 px-2">
+        <header>
           <div>
-            <h4>Monday June 10th</h4>
-            <ul>
-              <li>zone 1</li>
-            </ul>
+            <h1>H2O Time</h1>
+          </div>
+        </header>
+        <div id="schedule">
+          <h2>Schedule:</h2>
+        </div>
+        <div className="grow">
+          <h4>Monday June 10th</h4>
+          <ul>
+            <li>zone 1</li>
+          </ul>
+        </div>
+        <div id="zones">
+          <div>
+            <h2>Zones:</h2>
+            <button>+</button>
           </div>
         </div>
-      </div>
-      <div id="zones">
-        <div className="flex flex-row">
-          <h2>Zones:</h2>
-          <button>+</button>
-        </div>
-        <div>
+        <div className="grow">
           <img src="" alt="Warning" />
           <h4>Zone 1</h4>
           <span>every 3 days</span>
