a811bb3b28bd0c29bd13bb5127f46b030d1af474
Jaron Warren <jarwarren@gmail.com>
2023-07-24 13:07:48 -0600

    seperate dialog modal into compenent

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
-import { useRef } from "react";
 import "./App.css";
+import { ComponentZoneModal } from "./componentZoneModal";
 
 function App() {
-  let dialog = useRef<HTMLDialogElement>(null);
-
-  function addZone() {
-    // const dialog = document.getElementById("zoneModal") as HTMLDialogElement;
-    dialog.current?.showModal();
-  }
-
-  function closeModal() {
-    // const dialog = document.getElementById("zoneModal") as HTMLDialogElement;
-    dialog.current?.close();
-  }
-
-  function checkBounds(e: any) {
-    const dialogDimensions = dialog.current?.getBoundingClientRect();
-    if (
-      e.clientX < dialogDimensions.left ||
-      e.clientX > dialogDimensions.right ||
-      e.clientY < dialogDimensions.top ||
-      e.clientY > dialogDimensions.bottom
-    ) {
-      dialog.current?.close();
-    }
-  }
-
   return (
     <>
       <div className="mx-1 flex max-h-screen min-h-screen flex-col gap-2 px-2">
@@ -42,28 +18,10 @@ function App() {
         <div className="grow rounded-md border border-slate-600 bg-gradient-to-r from-cyan-500 to-blue-500">
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6"></div>
         </div>
-        <dialog
-          id="zoneModal"
-          ref={dialog}
-          onClick={(event) => checkBounds(event)}
-        >
-          <button
-            onClick={closeModal}
-            className="rounded-md border-2 border-slate-900 bg-red-500 px-1.5 pb-1 font-bold leading-3"
-          >
-            Close
-          </button>
-          <span>this is a dialog</span>
-        </dialog>
         <div id="zones">
           <div className="flex flex-row">
             <span className="whiteShadow mr-4 font-bold">Zones:</span>
-            <button
-              onClick={addZone}
-              className="rounded-md border-2 border-slate-900 bg-green-600 px-1.5 pb-1 font-bold leading-3"
-            >
-              +
-            </button>
+            <ComponentZoneModal />
           </div>
         </div>
         <div className="mb-3 grow rounded-md border border-slate-600 bg-gradient-to-r from-cyan-500 to-blue-500">
--- src/componentZoneModal.tsx
+++ src/componentZoneModal.tsx
@@ -1,5 +1,50 @@
-import React from "react";
+import { useRef } from "react";
 
-export function componentZoneModal() {
-  return <div>componentZoneModal</div>;
+export function ComponentZoneModal() {
+  let dialog = useRef<HTMLDialogElement>(null);
+
+  function showModal() {
+    dialog.current?.showModal();
+  }
+
+  function closeModal() {
+    // const dialog = document.getElementById("zoneModal") as HTMLDialogElement;
+    dialog.current?.close();
+  }
+
+  function checkBounds(e: any) {
+    const dialogDimensions = dialog.current?.getBoundingClientRect();
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      dialog.current?.close();
+    }
+  }
+
+  return (
+    <>
+      <button
+        onClick={showModal}
+        className="rounded-md border-2 border-slate-900 bg-green-600 px-1.5 pb-1 font-bold leading-3"
+      >
+        +
+      </button>
+      <dialog
+        id="zoneModal"
+        ref={dialog}
+        onClick={(event) => checkBounds(event)}
+      >
+        <button
+          onClick={closeModal}
+          className="rounded-md border-2 border-slate-900 bg-red-500 px-1.5 pb-1 font-bold leading-3"
+        >
+          Close
+        </button>
+        <span>this is a dialog</span>
+      </dialog>
+    </>
+  );
 }
