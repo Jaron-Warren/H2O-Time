9478c35b0cd059962cda7ffd6633efe3d64bc161
Jaron Warren <jarwarren@gmail.com>
2023-08-17 19:53:01 -0600

    calendar component local style

--- src/componentScheduleCalendar.tsx
+++ src/componentScheduleCalendar.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import "./componentScheduleDetails.css";
+import "./componentScheduleCalendar.css";
 
 type ValuePiece = Date | null;
 
