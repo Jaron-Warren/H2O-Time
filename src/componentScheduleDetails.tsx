2bdcbaa9693753c0bc116a82617f9f2ae9e930af
Jaron Warren <jarwarren@gmail.com>
2023-10-08 19:14:39 -0600

    today button tweak

--- src/componentScheduleDetails.tsx
+++ src/componentScheduleDetails.tsx
@@ -10,12 +10,14 @@ export function ComponentScheduleDetails({
   return (
     <>
       <div className="m-2 flex flex-col rounded-md border-2 border-slate-800 bg-slate-50 p-2">
-        <button
-          className="w-2/5 self-center rounded-sm border border-slate-600 bg-yellow-200"
-          onClick={() => changeActiveDate(new Date())}
-        >
-          Go To Today
-        </button>
+        {activeDate.toDateString() !== new Date().toDateString() && (
+          <button
+            className="w-2/5 self-center rounded-sm border border-slate-600 bg-yellow-200"
+            onClick={() => changeActiveDate(new Date())}
+          >
+            Go To Today
+          </button>
+        )}
         <h4 className="text-center underline">{activeDate.toDateString()}</h4>
         <ul className="list-inside list-disc py-1">
           {activeDateSchedule?.map((zone) => {
